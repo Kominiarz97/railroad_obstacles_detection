42ec7397b3d42b0fc0ee731e7e0a59f1d03f60ab
Kominiarz97 <kominiarz1997@gmail.com>
2020-05-08 19:02:58 +0200

    First part of detection

--- rails_detection.py
+++ rails_detection.py
@@ -2,6 +2,22 @@ import cv2
 import numpy as np
 import imutils
 from matplotlib import pyplot as plt
+
+def green_detection(img_crop):
+    area = 0.0
+    hsv_img_crop = cv2.cvtColor(img_crop, cv2.COLOR_BGR2HSV)
+    lower_green = np.array([40, 70, 80])
+    upper_green = np.array([70, 255, 255])
+    mask = cv2.inRange(hsv_img_crop, lower_green, upper_green)
+    cv2.imshow('maska', mask)
+    contours = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)[0]
+    for contour in contours:
+        area += cv2.contourArea(contour)
+    print(area)
+    cv2.drawContours(img_crop, contours, -1, (0, 0, 255), 3)
+    cv2.imshow('zielony', img_crop)
+
+
 tor_film=cv2.VideoCapture("mov/mov_4.mp4")
 kernel=np.ones((5,5),np.uint8)
 i=0
@@ -17,42 +33,33 @@ while(1):
     edges = cv2.Canny(blur, 230, 250)
     dilate=cv2.dilate(edges,kernel,iterations=2)
     erode=cv2.erode(dilate,kernel,iterations=2)
-    lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 70,minLineLength=200,maxLineGap=100)
+    lines = cv2.HoughLinesP(edges, 0.6, np.pi / 180, 70,minLineLength=200,maxLineGap=100)
     #cv2.imshow('krawedzie', edges)
-    for line in lines:
-        x1, y1, x2, y2 = line[0]
-        if x1<left_rail_start and x1>150:
-            left_rail_start=x1
-        if x1 > right_rail_start and x1>left_rail_start:
-            right_rail_start = x1
-        elif x2 > right_rail_start and x2>left_rail_start:
-            right_rail_start=x2
-        cv2.line(img, (x1, y1), (x2, y2), (255, 0, 0), 3)
-    img_crop = img[150:719, left_rail_start - 20:right_rail_start+20]
-    org_img_crop = org_img[150:719, left_rail_start - 20:right_rail_start+20]
-    #cv2.imshow('tory', img)
-    cv2.imshow('szyny',img_crop)
+    if lines is not None:
+        for line in lines:
+            x1, y1, x2, y2 = line[0]
+            if x1<left_rail_start and x1>150:
+                left_rail_start=x1
+            if x1 > right_rail_start and x1>left_rail_start:
+                right_rail_start = x1
+            elif x2 > right_rail_start and x2>left_rail_start:
+                right_rail_start=x2
+            cv2.line(img, (x1, y1), (x2, y2), (255, 0, 0), 3)
+        img_crop = img[150:719, left_rail_start - 20:right_rail_start+20]
+        org_img_crop = org_img[150:719, left_rail_start - 20:right_rail_start+20]
+        #cv2.imshow('tory', img)
+        cv2.imshow('szyny',img_crop)
+        green_detection(img_crop)
+        if i % 30 == 0:
+            plt.hist(org_img_crop.ravel(), 256, [0, 256])
+            plt.show()
 
-
-    area = 0.0
-    hsv_img_crop = cv2.cvtColor(img_crop, cv2.COLOR_BGR2HSV)
-    lower_green = np.array([40, 70, 80])
-    upper_green = np.array([70, 255, 255])
-    mask = cv2.inRange(hsv_img_crop, lower_green, upper_green)
-    cv2.imshow('maska', mask)
-    contours = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)[0]
-    for contour in contours:
-        area += cv2.contourArea(contour)
-    print(area)
-    cv2.drawContours(img_crop, contours, -1, (0, 0, 255), 3)
-    cv2.imshow('zielony', img_crop)
-    if i%30==0:
-        plt.hist(org_img_crop.ravel(), 256, [0, 256])
-        plt.show()
-
-
-    if cv2.waitKey(1)==27:
+        if cv2.waitKey(1) == 27:
+            break
+    else:
         break
+
+
 tor_film.release()
 cv2.destroyAllWindows()
 
--- rails_detection_photos.py
+++ rails_detection_photos.py
@@ -2,10 +2,9 @@ import cv2
 import numpy as np
 import imutils
 from matplotlib import pyplot as plt
-org_img=cv2.imread('photos/9.jpg')
+org_img=cv2.imread('photos/tree1.jpg')
 kernel=np.ones((5,5),np.uint8)
 area=0.0
-
 
 left_rail_start = 600
 top_rail_start=1000
@@ -16,7 +15,7 @@ blur = cv2.GaussianBlur(gray, (5, 5), cv2.BORDER_DEFAULT)
 edges = cv2.Canny(blur, 130, 250)
 dilate=cv2.dilate(edges,kernel,iterations=2)
 erode=cv2.erode(dilate,kernel,iterations=2)
-lines = cv2.HoughLinesP(erode, 1, np.pi / 180, 120,minLineLength=200,maxLineGap=100)
+lines = cv2.HoughLinesP(erode, 1, np.pi / 180, 120,minLineLength=50,maxLineGap=100)
 cv2.imshow('krawedzie', dilate)
 for line in lines:
     x1, y1, x2, y2 = line[0]
@@ -30,10 +29,9 @@ org_img_crop=org_img[top_rail_start:600,left_rail_start-50:left_rail_start+250]
 cv2.imshow('tory', img)
 cv2.imshow('szyny',img_crop)
 
-
 hsv_img_crop=cv2.cvtColor(img_crop,cv2.COLOR_BGR2HSV)
-lower_green=np.array([40,70,80])
-upper_green=np.array([70,255,255])
+lower_green=np.array([20,70,80])
+upper_green=np.array([90,255,255])
 mask=cv2.inRange(hsv_img_crop,lower_green,upper_green)
 cv2.imshow('maska',mask)
 contours=cv2.findContours(mask.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)[0]
